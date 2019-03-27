4557acd77b18a96d7673aabc45107004ebc1ff4e
lanyuzx <lanyuzx@qq.com>
2019-03-27 17:31:46 +0800

    增加redis数据缓存机制
    lanyu

--- e3-content/e3-content-service/src/main/java/cn/content/service/impl/LLContentServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/content/service/impl/LLContentServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.content.service.impl;
 
 import cn.content.service.LLContentService;
+import cn.e3mall.common.jedis.JedisClient;
 import cn.e3mall.common.pojo.LLPageCommon;
 import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
@@ -8,10 +9,14 @@ import cn.e3mall.pojo.TbContentExample;
 import cn.e3mial.common.utils.TaotaoResult;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.Date;
 import java.util.List;
 
@@ -20,8 +25,13 @@ public class LLContentServiceImpl implements LLContentService {
 
     @Autowired
     private TbContentMapper contentMapper;
-
-    private String  IMAGE_PATH_URL = "http://localhost:8081";
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${IMAGE_PATH_URL}")
+    private String  IMAGE_PATH_URL;
+    //缓存列表的key
+    @Value("${CONTENT_LIST_KEY}")
+    private  String CONTENT_LIST_KEY;
     @Override
     public TaotaoResult addContent(TbContent content) {
         content.setCreated(new Date());
@@ -57,6 +67,18 @@ public class LLContentServiceImpl implements LLContentService {
 
     @Override
     public List<TbContent> getAdContentList(long cid) {
+        //先查询缓存是否存在
+        try {
+            String contentJson = jedisClient.hget(IMAGE_PATH_URL, cid+"");
+            if (StringUtils.isNotBlank(contentJson)){
+                Type type = new TypeToken<List<TbContent>>(){}.getType();
+                List<TbContent> contentList = new Gson().fromJson(contentJson, type);
+                return  contentList;
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         TbContentExample contentExample = new TbContentExample();
         TbContentExample.Criteria criteria = contentExample.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
@@ -65,6 +87,13 @@ public class LLContentServiceImpl implements LLContentService {
         for ( TbContent contents : contentList) {
             String picUrl = IMAGE_PATH_URL + contents.getPic();
             contents.setPic(picUrl);
+        }
+        //添加缓存
+        try {
+            Long result  = jedisClient.hset(CONTENT_LIST_KEY, cid + "", new  Gson().toJson(contentList));
+            System.out.println( "存储结果 === " + result);
+        }catch (Exception e) {
+            e.printStackTrace();
         }
         return contentList;
     }
